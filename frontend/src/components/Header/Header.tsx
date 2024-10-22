9601aa988088ff5481112fc958838f9151d3db33
eunjju2 <graceun309@gmail.com>
2024-10-22 12:19:54 +0900

    🎨 Style : Header 마크업 구현 완료

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
-import logo from "@assets/images/quoteLogo.png";
 import DarkModeButton from "@assets/icons/darkMode_button.svg?react";
 import LightModeButton from "@assets/icons/lightMode_button.svg?react";
+import logo from "@assets/images/quoteLogo.png";
+import profile from "@assets/images/profile.png";
 import { useState } from "react";
 
 const HeaderContainer = styled.header`
@@ -10,10 +11,31 @@ const HeaderContainer = styled.header`
   height: 50px;
   opacity: 0.85;
   display: flex;
+  justify-content: space-between;
+  padding: 0 1rem;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 const Logo = styled.img`
   padding: 10px;
+`;
+
+const StyledDarkModeButton = styled(LightModeButton)`
+  width: 30px;
+  height: 30px;
+  margin-right: 1rem;
+  cursor: pointer;
+`;
+
+const StyledLightModeButton = styled(DarkModeButton)`
+  width: 30px;
+  height: 30px;
+  margin-right: 1rem;
+  cursor: pointer;
 `;
 
 const LoginButton = styled.button`
@@ -21,19 +43,42 @@ const LoginButton = styled.button`
   height: 30px;
   border-radius: 15px;
   border: 1px solid #474040;
+  color: #474040;
+  cursor: pointer;
+`;
+
+const Profile = styled.div`
+  width: 40px;
+  height: 40px;
+  margin-left: 1rem;
+  border-radius: 20px;
+  overflow: hidden;
+  cursor: pointer;
 `;
 
 const Header = () => {
-  const [mode, setMode] = useState<boolean>(true);
+  const [mode, setMode] = useState<boolean>(false);
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+
   return (
     <HeaderContainer>
       <Logo src={logo} />
-      {mode ? (
-        <DarkModeButton onClick={() => setMode(!mode)} width="30px" height="30px" />
-      ) : (
-        <LightModeButton onClick={() => setMode(!mode)} width="30px" height="30px" />
-      )}
-      <LoginButton />
+      <ButtonContainer>
+        {mode ? (
+          <StyledDarkModeButton onClick={() => setMode(!mode)} />
+        ) : (
+          <StyledLightModeButton onClick={() => setMode(!mode)} />
+        )}
+        <LoginButton>{isLogin ? "로그아웃" : "시작하기"} </LoginButton>
+        {isLogin && (
+          <Profile>
+            <img
+              src={profile}
+              width='100%'
+            />
+          </Profile>
+        )}
+      </ButtonContainer>
     </HeaderContainer>
   );
 };
