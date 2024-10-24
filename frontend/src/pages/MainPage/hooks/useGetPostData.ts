40c00b71ab84c330de53cd60f293599b43dcb28e
eunjju2 <graceun309@gmail.com>
2024-10-24 22:13:38 +0900

    ♻️ Refactor : 중복되는 기능 병합

--- frontend/src/pages/MainPage/hooks/useGetPostData.ts
+++ frontend/src/pages/MainPage/hooks/useGetPostData.ts
@@ -2,26 +2,7 @@ import { Post } from "@/types/Types";
 import { useQuery } from "@tanstack/react-query";
 import { getCategoryPostData, getPostData } from "../apis/postApi";
 
-//포스트 목록
-export const useGetPostData = () => {
-  const { data, isError, isLoading } = useQuery<Post[]>({
-    queryKey: ["postList"],
-    queryFn: () => getPostData(),
-  });
-
-  return { data, isLoading, isError };
-};
-
-//카테고리별 목록
-// export const useGetCategoryPostData = (category: string) => {
-//   const { data, isLoading, isError } = useQuery<Post[]>({
-//     queryKey: ["categoryPost", category],
-//     queryFn: () => getCategoryPostData(category),
-//   });
-
-//   return { data, isLoading, isError };
-// };
-
+//전체 및 카테고리별 포스트 목록
 export const useGetCategoryPostData = (category: string) => {
   const { data, isLoading, isError } = useQuery<Post[]>({
     queryKey: ["categoryPost", category],
