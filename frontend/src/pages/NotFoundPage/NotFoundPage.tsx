4147e96394ab8e72acc079ccd0557d9a50dfff0e
eunjju2 <graceun309@gmail.com>
2024-10-28 23:47:13 +0900

    ✨ Feat : NotFound 페이지 구현 완료

--- frontend/src/pages/NotFoundPage/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,78 @@
+import styled, { useTheme } from "styled-components";
+import NotFound_lightMode from "@assets/images/notFound_lightMode.png";
+import NotFound_darkMode from "@assets/images/notFound_darkMode.png";
+import MainLayout from "@/layouts/MainLayout";
+import { useNavigate } from "react-router-dom";
+
+const NotFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const LogoContainer = styled.img`
+  width: 150px;
+`;
+
+const ErrorTextContainer = styled.div`
+  font-size: 18px;
+  text-align: center;
+  line-height: 24px;
+  min-width: 500px;
+  color: ${({ theme }) => theme.colorButton};
+`;
+
+const ErrorTitle = styled.p`
+  font-size: 22px;
+  font-weight: bold;
+  margin: 20px;
+`;
+
+const GoHomeButton = styled.button`
+  margin: 30px;
+  width: 250px;
+  height: 50px;
+  background: none;
+  border-radius: 10px;
+  border: 1px solid ${({ theme }) => theme.colorButton};
+  color: ${({ theme }) => theme.colorButton};
+  font-size: 16px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colorButton};
+    color: ${({ theme }) => theme.colorSub};
+  }
+`;
+
 const NotFoundPage = () => {
-  return <>
-  
-  </>;
+  const theme = useTheme();
+
+  const navigate = useNavigate();
+  const handleGoHome = () => {
+    navigate("/");
+  };
+  return (
+    <MainLayout>
+      <NotFoundContainer>
+        <LogoContainer
+          src={
+            theme.mode === "lightMode" ? NotFound_lightMode : NotFound_darkMode
+          }
+        />
+        <ErrorTextContainer>
+          <ErrorTitle>페이지를 찾을 수 없습니다.</ErrorTitle>
+          페이지의 주소가 잘못 입력되었거나, <br />
+          주소가 변경 혹은 삭제되어 요청하신 페이지를 찾을 수 없습니다.
+        </ErrorTextContainer>
+        <GoHomeButton onClick={handleGoHome}>GO HOME</GoHomeButton>
+      </NotFoundContainer>
+    </MainLayout>
+  );
 };
 
 export default NotFoundPage;
--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,6 +1,7 @@
 import LoginPage from "@/pages/LogInPage/LoginPage";
 import MainPage from "@/pages/MainPage/MainPage";
 import MyPage from "@/pages/MyPages/MyPage";
+import NotFoundPage from "@/pages/NotFoundPage/NotFoundPage";
 import CreatePost from "@/pages/PostCreate/CreatePost";
 import PostDetailPage from "@/pages/PostDetail/PostDetailPage";
 import PostModifyPage from "@/pages/PostModify/PostModifyPage";
@@ -50,6 +51,10 @@ const router = createBrowserRouter([
   {
     path: "/user-page/:userId",
     element: <UserPage />,
+  },
+  {
+    path: "/error",
+    element: <NotFoundPage />,
   },
 ]);
 
